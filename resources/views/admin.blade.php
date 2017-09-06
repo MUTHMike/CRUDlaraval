95553c474435a16a07cb54ef6187dc431cbfb141
MUTHMike <mikemuth12@gmail.com>
2017-09-06 14:24:13 +0700

    chang style

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -511,10 +511,11 @@
    @yield('content')
   <!-- /.content-wrapper -->
 
+  
 <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{ asset('assetsAdmin/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ asset('assetsAdmin/bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
